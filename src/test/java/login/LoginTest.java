67081c95ee9345fe6e384dbeae3ce49f3bd1d012
Miguel Junior <miguel.ajr89@gmail.com>
2019-11-01 13:31:26 -0400

    Chapter 4.2 Add TestNG, Pattern Page Object, Upload the Readme.md

---
+++ src/test/java/login/LoginTest.java
@@ -0,0 +1,23 @@
+package login;
+
+import base.BaseTests;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import pages.SecureAreaPage;
+
+import static org.testng.Assert.assertTrue;
+
+
+public class LoginTest extends BaseTests {
+
+    @Test
+    public void testSuccessfulLogin(){
+        LoginPage loginPage = homePage.clickFormAuthentication();
+        loginPage.setUserName("tomsmith");
+        loginPage.setPassword("SuperSecretPassword!");
+        SecureAreaPage secureAreaPage = loginPage.clickLoginButton();
+        String messageSecureAreaPage = secureAreaPage.getAlertText();
+        assertTrue(messageSecureAreaPage.contains("You logged into a secure area!"),
+                "Alert Test is correct");
+    }
+}
