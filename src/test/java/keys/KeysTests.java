4c324fe8691d6c144f9e6a74a65bc22df63e820e
Miguel Junior <miguel.ajr89@gmail.com>
2019-11-01 17:54:56 -0400

    Chapter 6 only Example

---
+++ src/test/java/keys/KeysTests.java
@@ -0,0 +1,24 @@
+package keys;
+
+import base.BaseTests;
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class KeysTests extends BaseTests {
+
+    @Test
+    public void testBackspace(){
+        var KeyPage = homePage.clickKeyPresses();
+        KeyPage.enterText("A" + Keys.BACK_SPACE);
+        assertEquals(KeyPage.getResult(),"You entered: BACK_SPACE");
+    }
+    
+    @Test
+    public void testPI(){
+        var KeyPage = homePage.clickKeyPresses();
+        KeyPage.enterPi();
+        assertEquals(KeyPage.getResult(),"You entered: 4");
+    }
+}
