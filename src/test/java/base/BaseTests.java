78e13bd9b4860273483d87e7119cfe8a5da1ced1
Miguel Junior <miguel.ajr89@gmail.com>
2019-11-01 12:45:25 -0400

    Chapter 4.1

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -4,11 +4,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
 
 import java.util.List;
 
 public class BaseTests {
+
     private WebDriver driver;
+    protected HomePage homePage;
 
     public void setUp(){
         System.setProperty("webdriver.chrome.driver","resources/chromedriver");
@@ -16,15 +19,9 @@ public class BaseTests {
         driver.get("https://the-internet.herokuapp.com/");
         driver.manage().window().maximize();
 
-        WebElement shiftingContentLink = driver.findElement(By.linkText("Shifting Content"));
-        shiftingContentLink.click();
-        WebElement example1 = driver.findElement(By.linkText("Example 1: Menu Element"));
-        example1.click();
-        List<WebElement> optionsMenu = driver.findElements(By.tagName("li"));
-        System.out.println("Menus options are:" + optionsMenu.size());
+        homePage = new HomePage(driver);
 
-        System.out.println(driver.getTitle());
-        //driver.quit();
+        driver.quit();
     }
 
     public static void main(String[] args) {
